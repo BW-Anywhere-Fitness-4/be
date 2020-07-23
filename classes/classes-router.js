bb7d7368083bd58ef0d2beaea73138a5633128a9
mirage <mirage.kamran.pkt@gmail.com>
2020-07-23 16:10:45 -0700

    creating class model and router files

---
+++ classes/classes-router.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Classes = require("./classes-model");
+
+// retrieve all classes
+router.get("/api/classes", async (req, res, next) => {
+    try {
+        const courses = await Classes.findAll();
+        res.json(courses);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// retrieve a class with the specified id
+router.get("/api/classes/:id", async (req, res, next) => {
+    try {
+        const course = await Classes.findById(req.params.id);
+        res.json(course);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// creates a new class
+router.post("/api/classes", async (req, res, next) => {
+    try {
+        const course = await Classes.add(req.body);
+
+        res.status(201).json(course);
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const classesRouter = require("./classes/classes-router")
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to our API"
+    });
+});
+
+server.use(classesRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        message: "Something went wrong"
+    });
+});
+
+if (!module.parent) {
+    server.listen(port, () => {
+        console.log(`Running at http://localhost:${port}`);
+    });
+}
+
+module.exports = server;
